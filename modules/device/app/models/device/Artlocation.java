77164c8704f4bafec25d7fa51301d7e2a075d007
lengxia <1007705026@qq.comom>
2019-01-23 19:26:25 +0800

    v1.61

--- modules/device/app/models/device/Artlocation.java
+++ modules/device/app/models/device/Artlocation.java
@@ -2,12 +2,14 @@ package models.device;
 
 import com.avaje.ebean.Model;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.Date;
 
 /**
  * Created by lengxia on 2019/1/19.
  */
+@Entity
 public class Artlocation extends Model{
     @Id
     public Integer id;
