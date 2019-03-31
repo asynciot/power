bf8119f4b0d0be4c531f1d011203693e1c80c1b8
linhao0112 <15067653377@163.com>
2019-03-31 10:52:55 +0800

    Revert "v2.14"
    This reverts commit 04514e6c4e7186cd8d77038f7e060152f07457ff.

--- public/dist/53.async.js
+++ public/dist/53.async.js
@@ -1,1 +1,1 @@
-webpackJsonp([53],{SD4e:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var i=n("9wvh"),a=n.n(i),l=n("A1Y1"),r=(n.n(l),n("FIFv"));t.default=function(){return a.a.createElement(r.a,{type:"500",style:{minHeight:500,height:"80%"},linkElement:l.Link})}}});
+webpackJsonp([53],{AKeG:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var i=n("9wvh"),a=n.n(i),l=n("A1Y1"),r=(n.n(l),n("FIFv"));t.default=function(){return a.a.createElement(r.a,{type:"404",style:{minHeight:500,height:"80%"},linkElement:l.Link})}}});
