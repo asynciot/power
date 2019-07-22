05e7a76c95faa7d60b139aee7c9c4881cf5cc871
linhao0112 <15067653377@163.com>
2019-07-22 17:02:09 +0800

    Revert "Revert "v2.1.92""
    This reverts commit 66037096e54564738837d8f54ae47aa0c819fd0c.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -367,6 +367,23 @@ create table `order` (
   constraint pk_order primary key (id)
 );
 
+create table order_code (
+  id                            integer auto_increment not null,
+  name                          varchar(255),
+  code                          varchar(255),
+  resolution_id                 varchar(255),
+  constraint pk_order_code primary key (id)
+);
+
+create table order_resolution (
+  id                            integer auto_increment not null,
+  reason                        varchar(255),
+  answer                        varchar(255),
+  code_id                       varchar(255),
+  sign                          varchar(255),
+  constraint pk_order_resolution primary key (id)
+);
+
 create table organization (
   id                            integer auto_increment not null,
   leader                        varchar(255),
@@ -417,14 +434,6 @@ create table sms_record (
   code                          varchar(4) not null,
   timestamp                     datetime(6) not null,
   constraint pk_sms_record primary key (id)
-);
-
-create table tech (
-  id                            integer auto_increment not null,
-  name                          varchar(255),
-  reason                        varchar(255),
-  answer                        varchar(255),
-  constraint pk_tech primary key (id)
 );
 
 create index ix_account_create_time on account (create_time);
@@ -480,6 +489,10 @@ drop table if exists `offline`;
 
 drop table if exists `order`;
 
+drop table if exists order_code;
+
+drop table if exists order_resolution;
+
 drop table if exists organization;
 
 drop table if exists organize;
@@ -489,8 +502,6 @@ drop table if exists roles;
 drop table if exists runtime;
 
 drop table if exists sms_record;
-
-drop table if exists tech;
 
 drop index ix_account_create_time on account;
 drop index ix_sms_record_mobile on sms_record;
---
+++ modules/device/app/controllers/device/OrderCodeController.java
@@ -0,0 +1,61 @@
+package controllers.device;
+
+import akka.stream.Materializer;
+import akka.util.ByteString;
+import com.avaje.ebean.ExpressionList;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import controllers.common.BaseController;
+import controllers.common.CodeException;
+import controllers.common.ErrDefinition;
+import models.device.OrderResolution;
+import models.device.OrderCode;
+import play.Logger;
+import play.core.j.JavaResultExtractor;
+import play.data.DynamicForm;
+import play.data.FormFactory;
+import play.libs.Json;
+import play.libs.ws.WSClient;
+import play.mvc.Result;
+import com.avaje.ebean.SqlRow;
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Expr;
+import com.avaje.ebean.ExpressionList;
+import com.avaje.ebean.SqlRow;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by lengxia on 2018/10/30.
+ */
+public class OrderCodeController extends BaseController {
+    @Inject
+    private FormFactory formFactory;
+
+    @Inject
+    private Materializer mat;
+
+    public Result read(){
+        try {
+            DynamicForm form = formFactory.form().bindFromRequest();
+            ExpressionList<OrderResolution> exprList = OrderResolution.finder.where();
+            List<OrderResolution> codeList = new ArrayList<OrderResolution>();
+            String code_id = form.get("code");
+            if (code_id!=null&&!code_id.isEmpty()){
+                exprList=exprList.eq("sign",code_id);
+            }
+
+            codeList = exprList.findList();
+            return successList(codeList);
+        }
+        catch (Throwable e) {
+            e.printStackTrace();
+            Logger.error(e.getMessage());
+            return failure(ErrDefinition.E_COMMON_READ_FAILED);
+        }
+    }
+}
