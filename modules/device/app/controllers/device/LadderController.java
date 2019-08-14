3837626d0a7c9fc1d38bc434c887b826c74727c0
zeroenk <zyyukai@126.com>
2019-08-14 09:16:35 +0800

    v2.1.13

--- modules/device/app/controllers/device/LadderController.java
+++ modules/device/app/controllers/device/LadderController.java
@@ -3,10 +3,7 @@ package controllers.device;
 import akka.stream.Materializer;
 import akka.util.ByteString;
 import com.avaje.ebean.Ebean;
-import com.avaje.ebean.Expr;
 import com.avaje.ebean.ExpressionList;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -15,7 +12,6 @@ import controllers.common.BaseController;
 import controllers.common.CodeException;
 import controllers.common.ErrDefinition;
 
-import models.account.Account;
 import models.device.Cellocation;
 import models.device.DeviceInfo;
 import models.device.Ladder;
@@ -28,14 +24,12 @@ import play.core.j.JavaResultExtractor;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.data.FormFactory;
-import play.libs.Json;
 import play.libs.ws.WSClient;
 import play.mvc.Result;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -45,7 +39,6 @@ import java.util.Set;
 public class LadderController extends BaseController {
     @Inject
     private FormFactory formFactory;
-
     @Inject
     WSClient ws;
     @Inject
@@ -60,25 +53,29 @@ public class LadderController extends BaseController {
             if (form.hasErrors()) {
                 throw new CodeException(ErrDefinition.E_COMMON_FTP_INCORRECT_PARAM);
             }
-
             Ladder ladderInfo = form.get();
             if (ladderInfo.name == null || ladderInfo.name.isEmpty()) {
                 throw new CodeException(ErrDefinition.E_COMMON_FTP_INCORRECT_PARAM);
             }
-            Devices devices = new Devices();
+            Devices devices;
             if (!ladderInfo.ctrl.isEmpty()) {
                 devices = Devices.finder.where().eq("IMEI", ladderInfo.ctrl).findUnique();
-                ladderInfo.ctrl_id = devices.id.toString();
+                if (devices!=null){
+                    ladderInfo.ctrl_id = devices.id.toString();
+                }
             }else{
                 devices = Devices.finder.where().eq("IMEI", ladderInfo.door1).findUnique();
-                ladderInfo.ctrl_id = devices.id.toString();
+                if (devices!=null){
+                    ladderInfo.ctrl_id = devices.id.toString();
+                }
             }
             DeviceInfo deviceInfo = DeviceInfo.finder.where().eq("IMEI", devices.IMEI).findUnique();
-            ladderInfo.state = deviceInfo.state;
-            Ebean.save(ladderInfo);
-            deviceInfo.ladder_id = ladderInfo.id.toString();
-            Ebean.save(deviceInfo);
-
+            if(deviceInfo!=null){
+                ladderInfo.state = deviceInfo.state;
+                Ebean.save(ladderInfo);
+                deviceInfo.ladder_id = ladderInfo.id.toString();
+                Ebean.save(deviceInfo);
+            }
             return success();
         }
         catch (CodeException ce) {
@@ -101,13 +98,15 @@ public class LadderController extends BaseController {
             String search_info = form.get("search_info");
             String ladder_id = form.get("id");
             String group_id = form.get("group_id");
-            String register=form.get("register");
-            String tabcor = form.get("tagcolor");
+
+            String register = form.get("register");
+
+            String tag = form.get("tagcolor");
             String state = form.get("state");
             String pageStr = form.get("page");
             String numStr = form.get("num");
-            String follow=form.get("follow");
-            String install_addr=form.get("install_addr");
+            String follow = form.get("follow");
+            String install_addr = form.get("install_addr");
 
             if (ladder_id != null && !ladder_id.isEmpty()) {
                 Ladder ladder = Ladder.finder.byId(Integer.parseInt(ladder_id));
@@ -142,8 +141,8 @@ public class LadderController extends BaseController {
             if (state != null && !state.isEmpty()) {
                 exprList=exprList.contains("state",state);
             }
-            if (tabcor != null && !tabcor.isEmpty()) {
-                exprList=exprList.contains("tagcolor",tabcor);
+            if (tag != null && !tag.isEmpty()) {
+                exprList=exprList.contains("tagcolor",tag);
             }
             if(register!=null&&!register.isEmpty()){
                 exprList=exprList.eq("register",register);
@@ -162,8 +161,8 @@ public class LadderController extends BaseController {
                 throw new CodeException(ErrDefinition.E_COMMON_INCORRECT_PARAM);
             }
 
-            Integer page = Integer.parseInt(pageStr);
-            Integer num = Integer.parseInt(numStr);
+            int page = Integer.parseInt(pageStr);
+            int num = Integer.parseInt(numStr);
 
             ladderInfoList = exprList
                     .setFirstRow((page-1)*num)
@@ -198,10 +197,7 @@ public class LadderController extends BaseController {
             List<ObjectNode> nodeList = new ArrayList<ObjectNode>();
             for (JsonNode child : resultData.get("data").get("list")) {
                 ObjectNode node = (ObjectNode) new ObjectMapper().readTree(child.toString());
-                if(node.get("ctrl").asText() != ""){
-//                     Ladder ladder = Ladder.finder.where().eq("ctrl",node.get("ctrl").asText()).findUnique();
-//                     if(ladder !=null ) {
-//                         node.put("name", ladder.name);
+                if(!node.get("ctrl").asText().equals("")){
                         DeviceInfo deviceInfo = DeviceInfo.finder.where().eq("imei", node.get("ctrl").asText()).findUnique();
                         Devices devices = Devices.finder.where().eq("imei", node.get("ctrl").asText()).findUnique();
                         if (deviceInfo != null) {
@@ -228,12 +224,7 @@ public class LadderController extends BaseController {
                             node.put("t_logout", devices.t_logout.toString());
                             node.put("t_update", devices.t_update.toString());
                         }
-                    // }
                 }else {
-// 					Logger.info(node.get("door1").asText());
-//                     Ladder ladder = Ladder.finder.where().eq("door1", node.get("door1").asText()).findUnique();
-//                     if (ladder != null) {
-//                         node.put("name", ladder.name);
                         DeviceInfo deviceInfo = DeviceInfo.finder.where().eq("imei", node.get("door1").asText()).findUnique();
                         Devices devices = Devices.finder.where().eq("imei", node.get("door1").asText()).findUnique();
                         if (deviceInfo != null) {
@@ -256,7 +247,6 @@ public class LadderController extends BaseController {
                             node.put("ip_country", iplocation.country);
                             node.put("ip_region", iplocation.region);
                         }
-                    // }
                 }
                 nodeList.add(node);
             }
@@ -282,6 +272,9 @@ public class LadderController extends BaseController {
                 throw new CodeException(ErrDefinition.E_COMMON_INCORRECT_PARAM);
             }
             Ladder ladder=Ladder.finder.byId(Integer.parseInt(ladder_id));
+            if(ladder == null){
+                throw new CodeException(ErrDefinition.E_COMMON_INCORRECT_PARAM);
+            }
             if(name!=null&&!name.isEmpty()){
                 ladder.name=name;
             }
@@ -290,33 +283,33 @@ public class LadderController extends BaseController {
             }
             DeviceInfo deviceInfo = new DeviceInfo();
             if(type!=null&&!type.isEmpty()){
-                if(type.equals("1")){
-                    deviceInfo = DeviceInfo.finder.where().eq("IMEI", ladder.ctrl).findUnique();
-                    deviceInfo.ladder_id = null;
-                    Ebean.save(deviceInfo);
-                    ladder.ctrl=IMEI;
-                    deviceInfo = DeviceInfo.finder.where().eq("IMEI", ladder.ctrl).findUnique();
-                    deviceInfo.ladder_id = ladder_id;
-                    ladder.ctrl_id = deviceInfo.id.toString();
-                }else if(type.equals("2")){
-                    deviceInfo = DeviceInfo.finder.where().eq("IMEI", ladder.door1).findUnique();
-                    deviceInfo.ladder_id = null;
-                    Ebean.save(deviceInfo);
-                    ladder.door1=IMEI;
-                    deviceInfo = DeviceInfo.finder.where().eq("IMEI", ladder.door1).findUnique();
-                    deviceInfo.ladder_id = ladder_id;
-                    ladder.ctrl_id = deviceInfo.id.toString();
-                }else if(type.equals("3")){
-                    deviceInfo = DeviceInfo.finder.where().eq("IMEI", ladder.door2).findUnique();
-                    deviceInfo.ladder_id = null;
-                    Ebean.save(deviceInfo);
-                    ladder.door2=IMEI;
-                    deviceInfo = DeviceInfo.finder.where().eq("IMEI", ladder.door2).findUnique();
-                    deviceInfo.ladder_id = ladder_id;
-                    ladder.ctrl_id = deviceInfo.id.toString();
+                switch (type) {
+                    case "1":
+                        ladder.ctrl = IMEI;
+                        deviceInfo = DeviceInfo.finder.where().eq("IMEI", ladder.ctrl).findUnique();
+                        if (deviceInfo != null) {
+                            deviceInfo.ladder_id = ladder_id;
+                            ladder.ctrl_id = deviceInfo.id.toString();
+                        }
+                        break;
+                    case "2":
+                        ladder.door1 = IMEI;
+                        deviceInfo = DeviceInfo.finder.where().eq("IMEI", ladder.door1).findUnique();
+                        if (deviceInfo != null) {
+                            deviceInfo.ladder_id = ladder_id;
+                            ladder.ctrl_id = deviceInfo.id.toString();
+                        }
+                        break;
+                    case "3":
+                        ladder.door2 = IMEI;
+                        deviceInfo = DeviceInfo.finder.where().eq("IMEI", ladder.door2).findUnique();
+                        if (deviceInfo != null) {
+                            deviceInfo.ladder_id = ladder_id;
+                            ladder.ctrl_id = deviceInfo.id.toString();
+                        }
+                        break;
                 }
             }
-
             Ebean.save(deviceInfo);
             ladder.save();
             return success();
@@ -342,9 +335,10 @@ public class LadderController extends BaseController {
                 throw new CodeException(ErrDefinition.E_COMMON_INCORRECT_PARAM);
             }
             Ladder ladder=Ladder.finder.byId(Integer.parseInt(ladder_id));
-            ladder.group_id = id;
-
-            ladder.save();
+            if(ladder !=null){
+                ladder.group_id = id;
+                ladder.save();
+            }
             return success();
         }
         catch (CodeException ce) {
@@ -368,9 +362,10 @@ public class LadderController extends BaseController {
             }
 
             Ladder ladder=Ladder.finder.byId(Integer.parseInt(ladder_id));
-            ladder.group_id = "0";
-
-            ladder.save();
+            if(ladder !=null){
+                ladder.group_id = "0";
+                ladder.save();
+            }
             return success();
         }
         catch (CodeException ce) {
@@ -397,8 +392,10 @@ public class LadderController extends BaseController {
             DeviceInfo deviceInfo=DeviceInfo.finder.where()
                     .eq("ladder_id", ladder_id)
                     .findUnique();
-            deviceInfo.ladder_id=null;
-            Ebean.save(deviceInfo);
+            if (deviceInfo!=null){
+                deviceInfo.ladder_id=null;
+                Ebean.save(deviceInfo);
+            }
             Ebean.delete(ladder);
             return success();
         }
